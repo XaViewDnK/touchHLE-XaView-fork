d671d1f564d903c2b94bd0d0d1f4ca561d1ae084
whyineedname <emssaoig1@gmail.com>
2026-03-15 15:52:12 +0200

    Add files via upload

--- src/objc/classes.rs
+++ src/objc/classes.rs
@@ -437,6 +437,7 @@ fn substitute_classes(
         || name.starts_with("Mobclix")
         || name.starts_with("FB") // Facebook
         || name.starts_with("Flurry")
+        || name.starts_with("GK") // GameKit
         || name.starts_with("OpenFeint")
         || name.starts_with("Tapjoy"))
     {
